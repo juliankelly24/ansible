4aa504ce58154deebee8635a5964b10910b443a3
Julian Kelly <julian.kelly@aaf.edu.au>
2017-05-24 15:01:57 +1000

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+yum -y install git
+yum -y install ansible
+git clone https://github.com/juliankelly24/ansible ~/ansible
+
+function run_ansible {
+ansible-playbook -i 'localhost ansible_connection=local,' ~/ansible/create_users.yml
+}
