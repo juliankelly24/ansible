a01c08e6f6e20e97071db566ead7a84d17dfaebf
Julian Kelly <julian.kelly@aaf.edu.au>
2017-05-25 14:09:36 +1000

    Add files via upload

--- config.sh
+++ config.sh
@@ -1,10 +1,7 @@
+#!/bin/bash
 set -e
 
-yum -y install git
-yum -y install epel-release
-yum -y install ansible
-git clone https://github.com/juliankelly24/ansible ~/ansible
-
-function run_ansible {
-ansible-playbook -i 'localhost ansible_connection=local,' ~/ansible/create_users.yml
-}
+sudo yum -y install ansible
+sudo yum -y install git
+sudo git clone https://github.com/juliankelly24/ansible ~/ansible
+sudo ansible-playbook -i 'localhost ansible_connection=local,' ~/ansible/create_users.yml
