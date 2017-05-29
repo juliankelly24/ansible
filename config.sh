da72e836d7bbfaca83fcfaadc345602aea42a459
Julian Kelly <julian.kelly@aaf.edu.au>
2017-05-29 15:38:48 +1000

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
+REPO=/opt/aaf/repo
+
 yum -y install epel-release 
 yum -y install ansible git
-git clone https://github.com/juliankelly24/ansible /opt/aaf/repo
-ansible-playbook -i 'localhost ansible_connection=local,' /opt/aaf/repo/create_users.yml
+git clone https://github.com/juliankelly24/ansible $REPO
+ansible-playbook -i 'localhost ansible_connection=local,' $REPO/create_users.yml
