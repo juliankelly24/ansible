5d8010c9d4ba3ee8a916e7230de568e178bada68
Julian Kelly <julian.kelly@aaf.edu.au>
2017-05-29 15:17:17 +1000

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-sudo yum -y install ansible
-sudo yum -y install git
-sudo git clone https://github.com/juliankelly24/ansible ~/ansible
-sudo ansible-playbook -i 'localhost ansible_connection=local,' ~/ansible/create_users.yml
+yum -y install ansible git
+git clone https://github.com/juliankelly24/ansible ~/ansible
+ansible-playbook -i 'localhost ansible_connection=local,' ~/ansible/create_users.yml
