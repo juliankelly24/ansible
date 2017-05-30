a5fb1280477d12f04188a3e1d9849eea4b5bf798
Julian Kelly <julian.kelly@aaf.edu.au>
2017-05-30 18:57:00 +1000

    Delete cron.sh

--- cron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-REPO=/opt/aaf/cron
-REPO_NAME=https://github.com/juliankelly24/ansible
-
-git clone $REPO_NAME $REPO
-ansible-playbook -i 'localhost ansible_connection=local,' $REPO/create_users.yml
-rm -rf $REPO
