18da5305f70deeef7f03deecaab205be4766948e
Julian Kelly <julian.kelly@aaf.edu.au>
2017-05-30 14:04:32 +1000

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -4,4 +4,6 @@ set -e
 REPO=/opt/aaf/repo
 REPO_NAME=https://github.com/juliankelly24/ansible
 
+rm -rf $REPO
+git clone $REPO_NAME $REPO
 ansible-playbook -i 'localhost ansible_connection=local,' $REPO/create_users.yml
