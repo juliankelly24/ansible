b4177588da18dfb55d919ecc62f29966fee37546
Julian Kelly <julian.kelly@aaf.edu.au>
2017-05-30 19:02:48 +1000

    Update config.sh

--- config.sh
+++ config.sh
@@ -18,7 +18,14 @@ cd /opt/aaf/repo
 git reset --hard
 git pull
 ansible-playbook -i 'localhost ansible_connection=local,' $REPO/create_users.yml
-mkdir /opt/aaf/x
+
+
+if [[ ! -e $CRON_REPO ]]; then
+    mkdir /opt/aaf/x
+elif [[ ! -d $dir ]]; then
+    echo "CRON REPO ALREADY EXISTS" 1>&2
+fi
+
 cp -f /opt/aaf/repo/config.sh /opt/aaf/x/cron.sh
 chmod +x $CRON_REPO/cron.sh
 echo "30 1 * * * /bin/sh /opt/aaf/x/cron.sh > /root/cronlog" > /var/spool/cron/root
