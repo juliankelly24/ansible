569a8ce206f1d052816742a1bfb9daeca3ff6b4d
Julian Kelly <julian.kelly@aaf.edu.au>
2017-05-30 21:33:32 +1000

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
 set -e
 
+# File Locations
 REPO=/opt/aaf/repo
 CRON_REPO=/opt/aaf/x
 REPO_NAME=https://github.com/juliankelly24/ansible
 SSH=/home/ec2-user/.ssh/authorized_keys
 
+# Public User Keys Keys
 JULIAN=/opt/aaf/repo/pub_keys/julian.pub
 TERRY=/opt/aaf/repo/pub_keys/terry.pub
 DALIA=/opt/aaf/repo/pub_keys/dalia.pub
 MELROY=/opt/aaf/repo/pub_keys/melroy.pub
 
+# Installs Dependencies Epel & Ansible
 yum -y install epel-release
 yum -y install ansible git
 
+# Clones Repo if Directory does not exist
 if [[ ! -e $REPO ]]; then
     git clone $REPO_NAME $REPO
 elif [[ ! -d $dir ]]; then
      1>&2
 fi
 
+# Clears changes pulls updated repo master branch
 cd /opt/aaf/repo
 git reset --hard
 git pull
 
+# Creates Cron Directory if it doesn't exist
 if [[ ! -e $CRON_REPO ]]; then
     mkdir /opt/aaf/x
 elif [[ ! -d $dir ]]; then
      1>&2
 fi
 
+#Copies Cron File and overwrites any old files + Changes permissions to run shell
 cp -f /opt/aaf/repo/config.sh /opt/aaf/x/cron.sh
 chmod +x $CRON_REPO/cron.sh
 
+#Compares files to EC2-USER Public Key for Cron Issue Email Schedule
 if cmp -s "$JULIAN" "$SSH"
 then
   echo "* * * * * /bin/sh /opt/aaf/x/cron.sh | mail -s 'errors' julian.kelly@aaf.edu.au" > /var/spool/cron/root
@@ -54,4 +62,5 @@ then
   echo "Hello Melroy :)"
 fi
 
+# Run Ansible Playbook with Yum Update
 ansible-playbook -i 'localhost ansible_connection=local,' $REPO/create_users.yml
