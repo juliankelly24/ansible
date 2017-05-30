8068330e1317c62f98c60cad5f9110af2590d3eb
Julian Kelly <julian.kelly@aaf.edu.au>
2017-05-30 18:52:37 +1000

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,16 +6,18 @@ REPO_NAME=https://github.com/juliankelly24/ansible
 
 yum -y install epel-release
 yum -y install ansible git
-{
-if [ ! -f $REPO ];
-then
-    continue
-else
+
+if [[ ! -e $REPO ]]; then
     git clone $REPO_NAME $REPO
+elif [[ ! -d $dir ]]; then
+    echo "Git Pull" 1>&2
 fi
-}
+
 cd /opt/aaf/repo
-git pull
+git reset --hard
+git pull 
 ansible-playbook -i 'localhost ansible_connection=local,' $REPO/create_users.yml
-echo "30 1 * * * /bin/sh /opt/aaf/repo/config.sh > /root/cronlog" > /var/spool/cron/root
-chmod +x $REPO/config.sh
+cp -f /opt/aaf/repo/config.sh /opt/aaf/x/cron.#!/bin/sh
+echo "yum -y update" > /opt/aaf/x/cron.sh
+chmod +x $REPO/cron.sh
+echo "30 1 * * * /bin/sh /opt/aaf/x/cron.sh > /root/cronlog" > /var/spool/cron/root
