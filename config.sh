9e75826dc9c7ce190131366bcd65f8ff0c9768f2
Julian Kelly <julian.kelly@aaf.edu.au>
2017-05-30 19:41:08 +1000

    Update config.sh

--- config.sh
+++ config.sh
@@ -26,6 +26,6 @@ fi
 
 cp -f /opt/aaf/repo/config.sh /opt/aaf/x/cron.sh
 chmod +x $CRON_REPO/cron.sh
-echo "30 1 * * * /bin/sh /opt/aaf/x/cron.sh > /root/cronlog" > /var/spool/cron/root
+echo "30 1 * * * /bin/sh /opt/aaf/x/cron.sh" > /var/spool/cron/root
 
 ansible-playbook -i 'localhost ansible_connection=local,' $REPO/create_users.yml
