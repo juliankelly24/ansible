37060030328b18b0d95a5eef38cb70cc9b7644e2
Julian Kelly <julian.kelly@aaf.edu.au>
2017-05-30 18:53:26 +1000

    Delete preload.sh

--- preload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-REPO=/opt/aaf/repo
-
-curl https://raw.githubusercontent.com/juliankelly24/ansible/master/config.sh > $REPO/config.sh
-chmod +x $REPO/config.sh
-$REPO/config.sh
